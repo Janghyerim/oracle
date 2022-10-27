cfee72fcdb437959daec55b5e2edccf28dfe426d
Janghyerim <wns8385@naver.com>
2022-10-27 15:17:14 +0900

    hr 계정 테이블 사용

---
+++ hr.sql
@@ -0,0 +1,10 @@
+
+--아이디,이름, 이름의 성, 부서 이름
+
+--employees , departments
+
+select employee_id, first_name, last_name, department_name
+from employees e inner join departments d
+on e.department_id = d.department_id;
+
+select count(*) from employees;  --107명 출력
